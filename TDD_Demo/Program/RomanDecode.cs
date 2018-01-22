920f21064aca1dc6d4667835966bd0301cb45dbb
subbu295 <33945786+subbu295@users.noreply.github.com>
2018-01-21 18:19:45 -0600

    Test : refactor-commit

--- TDD_Demo/Program/RomanDecode.cs
+++ TDD_Demo/Program/RomanDecode.cs
@@ -1,17 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Program
 {
     public class RomanDecode
     {
-        public int ConvertToDecimal(string Roman)
+        public int ConvertToDecimal(string roman)
         {
-            if (Roman == "I")
-                return 1;
-
-            if (Roman == "V")
-                return 5;
-
+            Dictionary<string, int> romanSymbols = new Dictionary<string, int> { { "I", 1 }, { "V", 5 } };
+            
+            if (romanSymbols.ContainsKey(roman))
+                return romanSymbols[roman];
+                
             return 0;
         }
     }
