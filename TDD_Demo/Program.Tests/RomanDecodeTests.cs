e24a8f9962512df7904949593dccda76c56f57a8
subbu295 <33945786+subbu295@users.noreply.github.com>
2018-01-21 18:03:24 -0600

    Test : V - 5 : red-commit

--- TDD_Demo/Program.Tests/RomanDecodeTests.cs
+++ TDD_Demo/Program.Tests/RomanDecodeTests.cs
@@ -20,5 +20,19 @@ namespace Program.Tests
             // Assert
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        public void ConvertToDecimal_V_5()
+        {
+            var romanDecode = new RomanDecode();
+            var roman = "V";
+            var expected = 5;
+
+            // Act
+            var result = romanDecode.ConvertToDecimal(roman);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
     }
 }
